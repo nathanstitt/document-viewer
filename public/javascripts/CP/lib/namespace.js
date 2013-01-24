8a4c9d672351b2dce6d528417d184515f83c8e0c
Nathan Stitt <nathan@stitt.org>
2013-01-24 16:57:22 -0600

    Setup the dc.* name spaces for the annotation backbone classes

---
+++ public/javascripts/CP/lib/namespace.js
@@ -0,0 +1,13 @@
+(function(window, document, $, undefined) {
+
+  // Set up the necessary namespaces for the editing Backbone classes
+
+  window.dc || (window.dc = {});
+  window.dc.ui || (window.dc.ui = {});
+  window.dc.model || (window.dc.model = {});
+  window.dc.lib || (window.dc.lib = {});
+  
+})(window, document, jQuery);
+
+
+
